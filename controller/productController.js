1dcca9cf6af5078309545a74395a42cca652fb38
LuckyZacha <zachatatem@gmail.com>
2023-02-14 17:18:41 -0600

    Feat: scrape details

--- controller/productController.js
+++ controller/productController.js
@@ -1,16 +1,49 @@
 const {sendResponse} = require("../helper");
 const CrawlerService = require("../scrape/service");
+const {Configuration, OpenAIApi} = require("openai");
+
+const apiKey = process.env.OPENAI_API_KEY;
 
 const scrapeDetail = async (url) => {
   const Crawler = new CrawlerService();
-  await Crawler.init();
   try {
-    const seller = await Crawler.getSiteInfo(url);
-    const response = {
-      product: seller,
+    await Crawler.init();
+    let text = await Crawler.getText(url);
+
+    if (text.length > 500) {
+      text = text.substr(0, 500);
+    }
+
+    const configuration = new Configuration({
+      apiKey: apiKey,
+    });
+
+    const query = {
+      price:
+        text +
+        "\nQ: How much does the product cost? If it was sold out, what is the previous cost? Please put only number first\nA:",
+      title: text + "\nQ: What is title of the product? Please answer simply\nA:",
     };
 
+    const openai = new OpenAIApi(configuration);
+
+    let response = {};
+
+    for (const [key, value] of Object.entries(query)) {
+      const completion = await openai.createCompletion({
+        model: "text-davinci-003",
+        max_tokens: 2000,
+        n: 1,
+        prompt: value,
+      });
+
+      const answer = completion.data.choices[0].text.trim();
+      console.log("===========>", key, answer);
+      response[key] = answer;
+    }
+
     await Crawler.initBrowser();
+
     return {
       data: response,
       error: null,
@@ -31,6 +64,6 @@ module.exports = {
     const {data, error} = await scrapeDetail(url);
 
     if (error) return sendResponse(res, 500, error, data);
-    return sendResponse(res, 200, "Successfully fetched items", data);
+    return sendResponse(res, 200, "Successfully scraped detail of product", data);
   },
 };
