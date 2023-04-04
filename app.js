c14009b61ed701b69e72559c259bf21acdc66d7d
Sortoite <zachatatem@gmail.com>
2023-04-04 09:28:32 -0400

    Fix: CORS error

--- app.js
+++ app.js
@@ -14,11 +14,15 @@ const {authenticateToken} = require("./helper/index");
 
 // instantiate an express server
 const app = express();
+
 app.use((req, res, next) => {
   // Set CORS headers
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
   next();
 });
 
